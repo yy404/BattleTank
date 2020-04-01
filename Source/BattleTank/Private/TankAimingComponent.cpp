20ad1cb81edab62d2a027faaf25bb7f407075f60
yy404 <yuhui.yaocn@gmail.com>
2020-04-01 16:41:18 +0100

    Predict Projectile Landing Point

--- Source/BattleTank/Private/TankAimingComponent.cpp
+++ Source/BattleTank/Private/TankAimingComponent.cpp
@@ -42,5 +42,29 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 //    auto OurTankName = GetOwner()->GetName();
 //    auto BarrelLocation = Barrel->GetComponentLocation().ToString();
 //    UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s from %s"), *OurTankName, *HitLocation.ToString(), *BarrelLocation);
-    UE_LOG(LogTemp, Warning, TEXT("Firing at %f"), LaunchSpeed);
+    
+    if (!Barrel) { return; }
+    
+    FVector OutLaunchVelocity;
+    FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
+    
+    if (UGameplayStatics::SuggestProjectileVelocity
+        (
+            this,
+            OutLaunchVelocity,
+            StartLocation,
+            HitLocation,
+            LaunchSpeed,
+            false,
+            0,
+            0,
+            ESuggestProjVelocityTraceOption::DoNotTrace
+            )
+        ) // Calculate the OutLaunchVelocity
+    {
+        auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+        auto TankName = GetOwner()->GetName();
+        UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *TankName, *AimDirection.ToString());
+    }
+    // If no solution found do nothing
 }
